b41d0c43d047005785161700a2a46e175e9fe683
Scanu46 <nykb24@gmail.com>
2022-12-22 11:07:00 +0000

    javascript for quiz

---
+++ script.js
@@ -0,0 +1,117 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is the capital France?',
+    answers: [
+      { text: 'Paris', correct: true },
+      { text: 'Berlin', correct: false },
+      { text: 'Madrid', correct: false},
+      { text: 'Dublin', correct: false}
+
+    ]
+  },
+  {
+    question: 'What colours is the Irish flag made up of?',
+    answers: [
+      { text: 'Blue, White and Red', correct: false },
+      { text: 'Yellow and Red', correct: false },
+      { text: 'Green, white and orange', correct: true },
+      { text: 'Black, Yellow and Red', correct: false }
+    ]
+  },
+  {
+    question: 'What is the biggest country in Europe?',
+    answers: [
+      { text: 'Germany', correct: false },
+      { text: 'Russia', correct: true },
+      { text: 'Croatia', correct: false },
+      { text: 'Spain', correct: false }
+    ]
+  },
+  {
+    question: 'What country is shaped like a boot?',
+    answers: [
+      { text: 'Denmark', correct: false },
+      { text: 'Albania', correct: false },
+      { text: 'Poland', correct: false },
+      { text: 'Italy', correct: true }
+    ]
+  }
+]
