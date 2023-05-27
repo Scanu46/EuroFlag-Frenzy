1aa61a8c4a857509ac44ca8fbceade35dc174972
Scanu46 <nykb24@gmail.com>
2023-05-27 05:08:17 +0100

    revert

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('Equestion')
-const questionElement = document.getElementById('Iquestion')
+const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
